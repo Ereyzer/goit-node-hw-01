0fd187aeeda0a6de6ddc1a33b2c9702734d874be
Ivan Laver <ereyzer142@gmail.com>
2021-09-26 21:38:03 +0300

    add uiqid

--- contacts.js
+++ contacts.js
@@ -1,13 +1,9 @@
 const fs = require('fs/promises');
 const path = require('path');
+const uniqid = require('uniqid');
 
 const contactsPath = path.join(__dirname, './db/contacts.json');
 
-// contacts.js
-
-// TODO: add description
-
-//* this if result is god func return list with contacts
 async function listContacts() {
   try {
     const result = await fs.readFile(contactsPath, 'utf8');
@@ -19,7 +15,6 @@ async function listContacts() {
   }
 }
 
-//* this if result is god func return object with contact info
 async function getContactById(contactId) {
   try {
     const contacts = await listContacts();
@@ -31,7 +26,6 @@ async function getContactById(contactId) {
   }
 }
 
-//* this if result is god func return empty object
 async function removeContact(contactId) {
   const contacts = await listContacts();
   const newContactList = contacts.filter(({ id }) => id !== contactId);
@@ -46,11 +40,10 @@ async function removeContact(contactId) {
   }
 }
 
-//* this if result is god func return object with new contact info
 async function addContact(name, email, phone) {
   const contacts = await listContacts();
   const newContact = {
-    id: contacts[contacts.length - 1].id + 1,
+    id: uniqid(),
     name,
     email,
     phone,
